c4f84b199378d6b46c2b4b3986dbdb283ab14f29
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-08-15 14:28:12 -0300

    otro commit

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const { body, validationResult } = require("express-validator");
 
-
-
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/get", (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.post(
@@ -18,25 +16,20 @@ app.post(
     body("nya", "Ingrese nombre y apellido completo")
       .exists()
       .isLength({ min: 5 }),
-    body("email", "Ingrese un email válido").exists().isEmail(), // Use isEmail() to validate email format
+    body("email", "Ingrese un email válido").exists().isEmail(),
     body("edad", "Ingrese un valor numérico").exists().isNumeric(),
   ],
   (req, res) => {
-    /*const errors = validationResult(req);
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({errors;errors.array()});
-      console.log(errors)
-    }*/
-      
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-        console.log(req,body)
-        const valores = req.body
-        const validaciones = errors.array()
-        res.render('index,{validaciones,valores:valores})')
-      
+      console.log(req.body);
+      const valores = req.body;
+      const validaciones = errors.array();
+      return res.render("index", {
+        validaciones: validaciones,
+        valores: valores,
+      });
     } else {
-      // Use res.send() to send a response string
       return res.send("¡Validación exitosa!");
     }
   }
